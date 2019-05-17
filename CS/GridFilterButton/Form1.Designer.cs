0378b6a181502962c0c1a8fefd8fe31383aadea3
Torman999 <46162131+Torman999@users.noreply.github.com>
2019-05-17 18:04:17 +0300

    update example

--- CS/GridFilterButton/Form1.Designer.cs
+++ CS/GridFilterButton/Form1.Designer.cs
@@ -30,25 +30,14 @@ namespace GridFilterButton
         {
             this.components = new System.ComponentModel.Container();
             this.gridControl1 = new DevExpress.XtraGrid.GridControl();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this.carsDBDataSet = new GridFilterButton.CarsDBDataSet();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
-            this.colID = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colTrademark = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colModel = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colHP = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.carsTableAdapter = new GridFilterButton.CarsDBDataSetTableAdapters.CarsTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.carsDBDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // gridControl1
             // 
-            this.gridControl1.DataSource = this.bindingSource1;
             this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gridControl1.EmbeddedNavigator.Name = "";
             this.gridControl1.Location = new System.Drawing.Point(0, 0);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
@@ -57,62 +46,11 @@ namespace GridFilterButton
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
             // 
-            // bindingSource1
-            // 
-            this.bindingSource1.DataMember = "Cars";
-            this.bindingSource1.DataSource = this.carsDBDataSet;
-            // 
-            // carsDBDataSet
-            // 
-            this.carsDBDataSet.DataSetName = "CarsDBDataSet";
-            this.carsDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
             // gridView1
             // 
-            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
-            this.colID,
-            this.colTrademark,
-            this.colModel,
-            this.colHP});
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
             this.gridView1.CustomDrawColumnHeader += new DevExpress.XtraGrid.Views.Grid.ColumnHeaderCustomDrawEventHandler(this.gridView1_CustomDrawColumnHeader);
-            // 
-            // colID
-            // 
-            this.colID.Caption = "ID";
-            this.colID.FieldName = "ID";
-            this.colID.Name = "colID";
-            this.colID.Visible = true;
-            this.colID.VisibleIndex = 0;
-            // 
-            // colTrademark
-            // 
-            this.colTrademark.Caption = "Trademark";
-            this.colTrademark.FieldName = "Trademark";
-            this.colTrademark.Name = "colTrademark";
-            this.colTrademark.Visible = true;
-            this.colTrademark.VisibleIndex = 1;
-            // 
-            // colModel
-            // 
-            this.colModel.Caption = "Model";
-            this.colModel.FieldName = "Model";
-            this.colModel.Name = "colModel";
-            this.colModel.Visible = true;
-            this.colModel.VisibleIndex = 2;
-            // 
-            // colHP
-            // 
-            this.colHP.Caption = "HP";
-            this.colHP.FieldName = "HP";
-            this.colHP.Name = "colHP";
-            this.colHP.Visible = true;
-            this.colHP.VisibleIndex = 3;
-            // 
-            // carsTableAdapter
-            // 
-            this.carsTableAdapter.ClearBeforeFill = true;
             // 
             // Form1
             // 
@@ -124,8 +62,6 @@ namespace GridFilterButton
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.carsDBDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -135,13 +71,6 @@ namespace GridFilterButton
 
         private DevExpress.XtraGrid.GridControl gridControl1;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
-        private CarsDBDataSet carsDBDataSet;
-        private System.Windows.Forms.BindingSource bindingSource1;
-        private GridFilterButton.CarsDBDataSetTableAdapters.CarsTableAdapter carsTableAdapter;
-        private DevExpress.XtraGrid.Columns.GridColumn colID;
-        private DevExpress.XtraGrid.Columns.GridColumn colTrademark;
-        private DevExpress.XtraGrid.Columns.GridColumn colModel;
-        private DevExpress.XtraGrid.Columns.GridColumn colHP;
     }
 }
 
